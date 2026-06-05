66f49ed0d1467b4788fbd855ec345dd0d8b2eea7
sehrawatprajwal-wq <sehrawatprajwal@gmail.com>
2026-06-06 01:07:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,60 +1,50 @@
 function showMessage() {
 
-document.getElementById("message").innerHTML = `
-
-<br><br>
+document.getElementById(“message”).innerHTML = `
 
 <h2>Chapter 1 ✨</h2>
-
 <p>
 Two souls started their journeys separately.<br>
 One was Pihu.<br>
 One was Punnu.<br><br>
 
-Different paths.<br>
-Different stories.<br><br>
+Different paths.
+Different stories.
 
 Yet destiny had already written their names
 on the same page. ❤️
+
 </p>
-
-<br><br>
-
 <h2>Chapter 2 🌙</h2>
-
 <p>
 Pihu was calm.<br>
 Thoughtful.<br>
 Graceful.<br><br>
 
-Punnu was adventurous.<br>
-Spontaneous.<br>
-Always chasing horizons.<br><br>
+Punnu was adventurous.
+Spontaneous.
+Always chasing horizons.
 
-They weren't the same.<br>
-And maybe that was the magic.<br><br>
+They weren’t the same.
+And maybe that was the magic.
 
 Because sometimes two different souls
 fit together perfectly. ❤️
+
 </p>
-
-<br><br>
-
 <h2>Chapter 3 💫</h2>
-
 <p>
 Some conversations became memories.<br>
 Some moments became comfort.<br><br>
 
-Slowly, without realizing it,<br>
-they became each other's favourite place.<br><br>
+Slowly, without realizing it,
+they became each other’s favourite place.
 
-Not because life was perfect,<br>
-but because they chose each other.<br><br>
+Not because life was perfect,
+but because they chose each other.
 
 And that changed everything. ❤️
+
 </p>
 
 `;
-
-}
