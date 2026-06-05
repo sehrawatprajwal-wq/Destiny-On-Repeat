17b3342cdfa95fd760b1d0c77609f1a6aaa9167e
sehrawatprajwal-wq <sehrawatprajwal@gmail.com>
2026-06-06 00:37:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 function showMessage() {
-    document.getElementById("message").innerHTML =
-    "✨ Welcome to the story of Pihu & Punnu ❤️ ✨";
+
+document.getElementById("message").innerHTML = `
+<br><br>
+
+<h2>Chapter 1 ✨</h2>
+
+<p>
+Two souls started their journeys separately.<br>
+One was Pihu.<br>
+One was Punnu.<br><br>
+
+Different paths.<br>
+Different stories.<br><br>
+
+Yet destiny had already written their names
+on the same page. ❤️
+</p>
+
+`;
+
 }
