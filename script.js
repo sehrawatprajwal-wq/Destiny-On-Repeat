1e0e04a8613ff13eb76ff14b5cf4a4684e542553
sehrawatprajwal-wq <sehrawatprajwal@gmail.com>
2026-06-06 00:49:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function showMessage() {
 
 document.getElementById("message").innerHTML = `
+
 <br><br>
 
 <h2>Chapter 1 ✨</h2>
@@ -17,7 +18,28 @@ Yet destiny had already written their names
 on the same page. ❤️
 </p>
 
-`;
+<br><br>
 
+<h2>Chapter 2 🌙</h2>
+
+<p>
+
+Pihu was calm.<br>
+Thoughtful.<br>
+Graceful.<br><br>
+
+Punnu was adventurous.<br>
+Spontaneous.<br>
+Always chasing horizons.<br><br>
+
+They weren't the same.<br>
+
+And maybe that was the magic.<br><br>
+
+Because sometimes two different souls
+fit together perfectly. ❤️
+
+</p>
+
+`;
 }
-<h2>Chapter 1 ✨❤️</h2>
